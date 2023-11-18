b482c05ac1ac84695946278437d2db72b2a88fd7
Erdinç Dursun <erdincdursun20@gmail.com>
2023-11-18 09:11:47 +0300

    SignalR-Project
    SignalR-Project started.Project layers created

---
+++ SignalR.EntityLayer/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string TitleFirst { get; set; }
+        public string DescriptionFirst { get; set; }
+        public string TitleTwo { get; set; }
+        public string DescriptionTwo { get; set; }
+        public string TitleThree { get; set; }
+        public string DescriptionThree { get; set; }
+    }
+}
