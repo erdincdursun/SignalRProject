4daeff25be51779664c53b84ad8a681983a240cb
Erdinç Dursun <erdincdursun20@gmail.com>
2023-11-19 19:54:38 +0300

    Data transfer object writted

---
+++ SignalR.DtoLayer/FeatureDto/GetFeatureDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.FeatureDto
+{
+    public class GetFeatureDto
+    {
+        public int FeatureId { get; set; }
+        public string TitleFirst { get; set; }
+        public string DescriptionFirst { get; set; }
+        public string TitleTwo { get; set; }
+        public string DescriptionTwo { get; set; }
+        public string TitleThree { get; set; }
+        public string DescriptionThree { get; set; }
+    }
+}
